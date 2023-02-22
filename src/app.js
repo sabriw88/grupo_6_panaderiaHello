8e15e65af0bde3a9f23ad573fa66ae5417dfe5f3
sabriw88 <sabrina.wf@gmail.com>
2023-02-21 22:42:35 -0300

    Subo estructura de carpetas, home y login

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, () => {console.log("Server running in port 3000")});
+
+app.use(express.static(path.join(__dirname,'../public')));
+console.log(path.join(__dirname,'../public'));
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, '/views/login.html'))});
+app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, '/views/register.html'))})
---
+++ src/styles.md
@@ -0,0 +1,8 @@
+Colores
+010101
+F1C732
+68ABB4
+
+Tipografías:
+Meatloaf
+AngryBirds
