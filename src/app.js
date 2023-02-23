53e3619158e231deba8270210ea7954bc7b18e24
jgabrielmedina <jogabrielmedina@gmail.com>
2023-02-23 09:44:56 -0300

    preparando las views para codear

--- src/app.js
+++ src/app.js
@@ -10,3 +10,5 @@ console.log(path.join(__dirname,'../public'));
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
 app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, '/views/login.html'))});
 app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, '/views/register.html'))})
+app.get('/productCart', (req, res) => {res.sendFile(path.join(__dirname, '/views/productCart.html'))})
+app.get('/productDetail', (req, res) => {res.sendFile(path.join(__dirname, '/views/productDetail.html'))})
