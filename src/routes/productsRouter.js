ae0b6c828ea310e185d72cba08a5b5dee4ee48c6
sabriw88 <sabrina.wf@gmail.com>
2023-03-19 10:55:00 -0300

    link a productos nuevamente modificado

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const productsController = require("../controllers/productsController.js");
 
-router.get('/product', productsController.listaDeProductos);
+router.get('/', productsController.listaDeProductos);
 router.get('/productDetail', productsController.detalleProducto);
 router.get('/productCreate', productsController.create);
 
