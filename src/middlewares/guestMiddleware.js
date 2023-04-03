762fad1750e2a6f6453437aef6d6c23244b8f57b
sabriw88 <sabrina.wf@gmail.com>
2023-04-03 20:58:32 -0300

    Subo registro de usuario + login + hash + session + cookies

---
+++ src/middlewares/guestMiddleware.js
@@ -0,0 +1,8 @@
+function guestMiddleware(req, res, next) {
+    if (req.session.loggedUser) {
+        return res.redirect('/users/profile');
+    }
+    next();
+}
+
+module.exports = guestMiddleware;
---
+++ src/middlewares/multerMiddleware.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/img/avatars');
+    },
+    filename: (req, file, cb) => {
+        let fileName = Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
+    }
+})
+
+const uploadFile = multer({ storage });
+
+module.exports = uploadFile;
+
