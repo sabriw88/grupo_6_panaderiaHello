d127c26fe022f3f56833bdb0ef789f99b9604c82
jgabrielmedina <jogabrielmedina@gmail.com>
2023-06-03 14:08:51 -0300

    validacion contrasenia back

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -9,7 +9,28 @@ const validations = [
         .isEmail().withMessage('Debe ingresar un correo electrónico válido'),
     body('bday').notEmpty().withMessage('Debe ingresar su fecha de nacimiento'),
     body('address').notEmpty().withMessage('Debe ingresar su dirección'),
-    body('password').notEmpty().withMessage('Debe ingresar una contraseña'),
+
+//validacion de contraseña
+    body('password')
+    .notEmpty().withMessage('Debe ingresar una contraseña').bail()
+    .isLength({min:8}).withMessage('Debera tener al menos 8 caracteres').bail()
+    .matches("[A-Z]").withMessage('Debera tener al menos 1 letra mayuscula').bail()
+    .matches("[0-9]").withMessage('Debera tener al menos 1 numero').bail()
+    .matches("[a-z]").withMessage('Debera tener al menos 1 letra minuscula'), 
+    
+
+   /*  body("confirmPsw")
+    .notEmpty()
+    .withMessage("Este campo no puede estar vacío.")
+    .custom((value, { req }) => {
+      if (value === req.body.password) {
+        return true;
+      }
+      return false;
+    })
+    .withMessage("Las contraseñas no coinciden."), */
+
+
     body('avatar').custom((value, {req}) => {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.gif'];
