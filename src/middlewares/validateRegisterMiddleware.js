536e03aa4b7c56d276d7fa5e6fb298487ac61ca4
sabriw88 <sabrina.wf@gmail.com>
2023-06-12 11:00:25 -0300

    revisión validación register (back)

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -2,31 +2,36 @@ const path = require('path');
 const { body } = require('express-validator');
 
 const validations = [
-    body('name').notEmpty().withMessage('Debe ingresar su nombre'),
-    body('surname').notEmpty().withMessage('Debe ingresar su apellido'),
+    body('name')
+        .notEmpty().withMessage('Debe ingresar su nombre').bail()
+        .isLength({min:2}).withMessage('Revise que su nombre esté completo'),
+    body('surname')
+        .notEmpty().withMessage('Debe ingresar su apellido').bail()
+        .isLength({min:2}).withMessage('Revise que su apellido esté completo'),
     body('email')
         .notEmpty().withMessage('Debe ingresar su correo electrónico').bail()
-        .isEmail().withMessage('Debe ingresar un correo electrónico válido'),
+        .isEmail().withMessage('Debe ingresar un correo electrónico válido')
+        /* validar q no se repita con un mail de la base */,
     body('bday').notEmpty().withMessage('Debe ingresar su fecha de nacimiento'),
     body('address').notEmpty().withMessage('Debe ingresar su dirección'),
 
 //validacion de contraseña
   body('password')
-    .notEmpty().withMessage('').bail()
-    .isLength({min:8}).withMessage('Debera tener al menos 8 caracteres').bail()
-    .matches("[A-Z]").withMessage('Debera tener al menos 1 letra mayuscula').bail()
-    .matches("[0-9]").withMessage('Debera tener al menos 1 numero').bail()
-    .matches("[a-z]").withMessage('Debera tener al menos 1 letra minuscula'),  
+    .notEmpty().withMessage('Debe ingresar una contraseña').bail()
+    .isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres').bail()
+    .matches("[A-Z]").withMessage('La contraseña debe tener al menos 1 letra mayúscula').bail()
+    .matches("[0-9]").withMessage('La contraseña debe tener al menos 1 número').bail()
+    .matches("[a-z]").withMessage('La contraseña debe tener al menos 1 letra minúscula'),  
 
     body('avatar').custom((value, {req}) => {
         let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png', '.gif'];
+        let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
         if (!file) {
             throw new Error ('Debe subir una imagen')
         } else {
             let fileExtension = path.extname(file.originalname);
             if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error ('Las extensiones de archivo permitidas son .jpg, .png y .gif');
+                throw new Error ('Las extensiones de archivo permitidas son .jpg, .jpeg, .png y .gif');
             }
         }
         return true;
