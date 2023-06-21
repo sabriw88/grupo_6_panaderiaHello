631476b228ac2c39c9a32d1fb986462ecea95f88
sabriw88 <sabrina.wf@gmail.com>
2023-06-21 18:45:59 -0300

    validación mail (front) en el registro completo

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -1,68 +1,60 @@
-console.log('Archivo registerValidation vinculado');
-
 window.addEventListener('load', function(){
 
     let formulario = document.querySelector('form.register-input');
+    let campoNombre = document.querySelector('#nombre');
+    let campoApellido = document.querySelector('#apellido');
+    let campoMail = document.querySelector('input.email');
+    let regEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    let campoContraseña = document.querySelector("input.contraseña");
+    let imagenProducto = document.querySelector('#foto-usuario');
+    let allowedFormats = ['jpg', 'jpeg', 'png', 'gif'];
+    let fileExtension = imagenProducto.value.split('.').pop().toLowerCase();
 
     formulario.addEventListener('submit', function(event){
-        
         let errores = [];
 
     //validacion del nombre
-
-            let campoNombre = document.querySelector('#nombre');
-
             if(campoNombre.value == ''){
                 errores.push('El campo "Nombre" no puede estar vacío');
             }else if(campoNombre.value.length < 2){
                 errores.push('El campo "Nombre" debe tener al menos 2 caracteres');
             }
 
-            let campoApellido = document.querySelector('#apellido');
-
+    // validación del apellido
             if(campoApellido.value == ''){
                 errores.push('El campo "Apellido" no puede estar vacío');
             }else if(campoApellido.value.length < 2){
                 errores.push('El campo "Apellido" debe tener al menos 2 caracteres');
             }
 
+    //validacion email front
+            if (campoMail.value == "") {
+                errores.push('El campo "Email" no puede estar vacío')
+            } else if (!regEmail.test(campoMail.value)) {
+                errores.push('Debe ingresar un correo electrónico válido')
+            }
 
     //validacion contrasena
-
-        let campoContraseña = document.querySelector("input.contraseña")
-
-        if (campoContraseña.value == "") {
-            errores.push("el campo de contraseña no tiene que estar vacio")
-        } else if (campoContraseña.value.length < 8) {
-            errores.push("la contraseña debe tener al menos 8 caracteres, una letra mayuscula y una minuscula")
-        }
-
+            if (campoContraseña.value == "") {
+                errores.push('El campo de "Contraseña" no tiene que estar vacio')
+            } else if (campoContraseña.value.length < 8) {
+                errores.push("La contraseña debe tener al menos 8 caracteres, una letra mayúscula y una minúscula")
+            }
 
     //validacion imagen front 
-    let imagenProducto = document.querySelector('#foto-usuario');
-    let allowedFormats = ['jpg', 'jpeg', 'png', 'gif'];
-    let fileExtension = imagenProducto.value.split('.').pop().toLowerCase();
-    
-    if (imagenProducto.value !== '' && !allowedFormats.includes(fileExtension)) {
-        errores.push('El campo "Imagen" debe ser en formato JPG, JPEG, PNG o GIF');
-    }   
-    
-
-    //validacion email front
-
+            if (imagenProducto.value !== '' && !allowedFormats.includes(fileExtension)) {
+                errores.push('El campo "Imagen" debe ser en formato JPG, JPEG, PNG o GIF');
+            }   
 
 
     //imprimiendo errores en el div 
-
-        if(errores.length > 0){
-            event.preventDefault();
-
-            let ulErrores = document.querySelector('div.errores ul');
-            ulErrores.innerHTML = ''; // Limpiar los errores anteriores
-            for (let i = 0; i < errores.length; i++) {
-                ulErrores.innerHTML += '<li>' + errores[i] + '</li>'
-                
-            }
-        } 
+            if(errores.length > 0){
+                event.preventDefault();
+                let ulErrores = document.querySelector('div.errores ul');
+                ulErrores.innerHTML = ''; // Limpiar los errores anteriores
+                for (let i = 0; i < errores.length; i++) {
+                    ulErrores.innerHTML += '<li>' + errores[i] + '</li>'
+                }
+            } 
     })
 })
